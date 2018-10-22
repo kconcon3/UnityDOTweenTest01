0b13fe9838f22f51b0864951dfcb7b90918407ca
kconcon3 <40970014+kconcon3@users.noreply.github.com>
2018-10-22 19:07:12 +0900

    Initial commit

---
+++ Assets/Scripts/Test01.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Test01 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 下記はどちらを使っても同じ
+#if true
+		gameObject.transform.DOMove(new Vector3(400.0f, 0.0f, 0.0f), 1.0f);
+#else
+		DOTween.To(
+			() => gameObject.transform.position,	// getter
+			x => transform.position = x,			// setter
+			new Vector3(400.0f, 0.0f, 0.0f),		// to
+			1.0f									// duration
+		);
+#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# DOTweenTest01
+
+## 概要  
+UnityのDOTweenを試してみる  
+<br /> 
+## テストした時の環境  
+Windows7 Pro 64bit  
+Unity 2018.2.10f1  
+Visual Studio Community 2017  
+DOTween v1.2.055 (Release build)  
+
+## 詳細
+[http://kconcon3.hatenablog.com/entry/2018/10/22/185655](http://kconcon3.hatenablog.com/entry/2018/10/22/185655) を参照して下さい
+
