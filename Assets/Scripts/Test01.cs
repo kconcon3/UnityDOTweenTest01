d530fe79e5bf958e4c65bed80dca711a2b1ce2a0
kconcon3 <hkane3sub@excite.co.jp>
2018-10-23 14:19:31 +0900

    コメントと変数名を変更

--- Assets/Scripts/Test01.cs
+++ Assets/Scripts/Test01.cs
@@ -12,10 +12,10 @@ public class Test01 : MonoBehaviour {
 		gameObject.transform.DOMove(new Vector3(400.0f, 0.0f, 0.0f), 1.0f);
 #else
 		DOTween.To(
-			() => gameObject.transform.position,	// getter
-			x => transform.position = x,			// setter
-			new Vector3(400.0f, 0.0f, 0.0f),		// to
-			1.0f									// duration
+			() => gameObject.transform.position,	// 何に
+			(n) => transform.position = n,			// 何を
+			new Vector3(400.0f, 0.0f, 0.0f),		// どこまで(最終的な値)
+			1.0f									// 時間
 		);
 #endif
 	}
